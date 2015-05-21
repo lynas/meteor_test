a767fa317ed2c7afb4281b2eac8cee5be5c2b733
sazzad <sazzad@ipvision-inc.com>
2015-05-21 10:42:06 +0600

    initial commit with basic routing done

---
+++ client/router.js
@@ -0,0 +1,9 @@
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
+Router.map(function(){
+    this.route('home', {path:'/'}),
+    this.route('login', {path:'/login'}),
+    this.route('register', {path:'/register'})
+});
