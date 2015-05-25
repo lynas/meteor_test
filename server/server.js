55cecab989423d03e53151821237eececa574ae0
sazzad <sazzad@ipvision-inc.com>
2015-05-25 15:56:44 +0600

    gmail added, need to add send mail verification code

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+process.env.MAIL_URL="smtp://ipv.sazzad@gmail.com:testipvision@smtp.gmail.com:465/";
+
+/*Email.send({
+    from: "meteor.email.2014@gmail.com",
+    to: "ipv.sazzad@gmail.com",
+    subject: "Meteor Can Send Emails via Gmail",
+    text: "Its pretty easy to send emails via gmail."
+});*/
 User_Profile = new Meteor.Collection('user_profile');
 if (Meteor.isServer) {
     Meteor.startup(function () {
